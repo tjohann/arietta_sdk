d3b15f9502979f3a99e8424e322972277e5b938f
Thorsten Johannvorderbrueggen <thorsten.johannvorderbrueggen@t-online.de>
2017-05-18 15:42:19 +0200

    update external kernel to 4.9.28 and 4.9.27-rt18

--- configs/arietta_env.sh
+++ configs/arietta_env.sh
@@ -6,9 +6,9 @@
 export MY_HOST_ARCH=$(uname -m)
 
 # set supported kernel version
-export ARIETTA_KERNEL_VER=4.9.23
-export ARIETTA_RT_KERNEL_VER=4.9.20
-export ARIETTA_RT_VER=rt16
+export ARIETTA_KERNEL_VER=4.9.28
+export ARIETTA_RT_KERNEL_VER=4.9.27
+export ARIETTA_RT_VER=rt18
 
 # home of the git repo
 export ARIETTA_HOME=/var/lib/arietta_sdk
